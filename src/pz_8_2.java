a55abad9fafeaec27b0fc8e26a9f778f5f3465bc
divan <hlinakcurbag@gmail.com>
2023-09-21 21:26:24 +0300

    исправила вывод; добавила пояснялки для ввода

--- src/pz_8_2.java
+++ src/pz_8_2.java
@@ -1,29 +1,33 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class pz_8_2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите n: ");
         int n = scanner.nextInt();
 
-        String[] lst = new String[n];
-
+        ArrayList<String> list = new ArrayList<>();
         for (int i = 0; i < n; i++) {
-            lst[i] = String.valueOf(i);
+            list.add(String.valueOf(i));
         }
 
+        System.out.println("Введите m (m <= n): ");
         int m = scanner.nextInt();
+        System.out.println("Введите строчечку: ");
         String s = scanner.next();
 
-        lst[m] = s;
+        list.add(m, s);
 
-        for (String num : lst) {
-            System.out.print(num + " ");
-        }
+        System.out.println("Вывод");
+        System.out.println(String.join(" ", list));  // 0 1 Строка 2 3 4
     }
 }
 /*
 Добавление элемента
-Напишите программу, которая считает натуральное число n c клавиатуры и создаст список, в котором будут хранится строковые представления чисел от 0 до n-1. Затем программа считывает число m (m <= n) и строку s. Вставьте строку s на позицию m. Выведите список на экран через пробел.
+Напишите программу, которая считает натуральное число n c клавиатуры и создаст список, в котором будут хранится
+строковые представления чисел от 0 до n-1. Затем программа считывает число m (m <= n) и строку s. Вставьте строку s на
+позицию m. Выведите список на экран через пробел.
 Ввод
 5
 2
