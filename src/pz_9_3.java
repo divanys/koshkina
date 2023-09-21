e9595d967d40d51323227348d18cf1e89a8cc30f
divan <hlinakcurbag@gmail.com>
2023-09-21 16:14:03 +0300

    4/5 9

--- src/pz_9_3.java
+++ src/pz_9_3.java
@@ -1,9 +1,28 @@
+import java.util.*;
+
 public class pz_9_3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("сколько");
+        int n = scanner.nextInt();
+
+        Set<Integer> seenNumbers = new HashSet<>();
+        System.out.println("что");
+
+        for (int i = 0; i < n; i++) {
+            int num = scanner.nextInt();
+            if (seenNumbers.contains(num)) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+                seenNumbers.add(num);
+            }
+        }
+    }
 }
 
 /*
 Встречалось ли число раньше?
 Вводится число n, затем n целых чисел. Для каждого числа выведите слово YES (в отдельной строке),
 если это число ранее встречалось в последовательности или NO,  если не встречалось.
-
 */
--- src/pz_9_4.java
+++ src/pz_9_4.java
@@ -1,6 +1,27 @@
-public class pz_9_4 {
-}
+import java.util.*;
 
+public class pz_9_4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("сколько");
+        int numLines = scanner.nextInt();
+        scanner.nextLine();
+
+        Set<String> uniqueWords = new HashSet<>();
+        System.out.println("что");
+
+        for (int i = 0; i < numLines; i++) {
+            String line = scanner.nextLine();
+            String[] words = line.split("\\s+");
+
+            for (String word : words) {
+                uniqueWords.add(word);
+            }
+        }
+
+        System.out.println("Количество различных слов: " + uniqueWords.size());
+    }
+}
 /*
 Количество слов
 Дан текст: в первой строке записано число строк, далее идут сами строки. Определите, сколько различных слов
