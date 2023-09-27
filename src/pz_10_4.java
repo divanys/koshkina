68a8edf8c3c60267fbebc5be43e5fbabb792b67f
divan <hlinakcurbag@gmail.com>
2023-09-27 18:48:39 +0300

    bubbles and 4/5 pz

--- src/pz_10_4.java
+++ src/pz_10_4.java
@@ -1,31 +1,38 @@
-//// Напишите функцию sortByLength, которая принимает как аргумент строку и сортирует её по длине слова.
-//// Если длины слов одинаковы, то сортировать в алфавитном порядке. Содержание функции main менять запрещено.
-////
-////
-////
-////
-//
-//
-//import java.util.Scanner;
-//import java.util.Arrays;
-//
-//public class pz_10_4 {
-//    public static void main(String[] args) {
-//        Scanner scanner = new Scanner(System.in);
-//
-//        System.out.println("Введите число для проверки (a): ");
-//        int a = scanner.nextInt();
-//
-//        System.out.println(sortByLength(a) ? "YES" : "NO");
-//    }
-//
-//    public static boolean sortByLength(String ) {
-//        if (number < 2) return false;
-//        for (int i = 2; i <= number / 2; i++) {
-//            if (number % i == 0) {
-//                return false;
-//            }
-//        }
-//        return true;
-//    }
-//}
+// Напишите функцию sortByLength, которая принимает как аргумент строку и сортирует её по длине слова.
+// Если длины слов одинаковы, то сортировать в алфавитном порядке. Содержание функции main менять запрещено.
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class pz_10_4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите строку: ");
+        String input = scanner.nextLine();
+        String sortedString = sortByLength(input);
+        System.out.println("Отсортированная строка: ");
+        System.out.println(sortedString);
+    }
+
+    // я не поняла ваших спойлеров, запуталась, вспомнила, что обсуждали с вами сортировку пузырьком, пропузырила код
+    // короче, не бейте
+
+    public static String sortByLength(String input) {
+        String[] words = input.split(" ");
+
+        for (int i = 0; i < words.length - 1; i++) {
+            for (int j = 0; j < words.length - i - 1; j++) {
+                if (words[j].length() > words[j + 1].length() ||
+                        (words[j].length() == words[j + 1].length() && words[j].compareTo(words[j + 1]) > 0)) {
+                    String temp = words[j];
+                    words[j] = words[j + 1];
+                    words[j + 1] = temp;
+                }
+            }
+        }
+
+        return String.join(" ", words);
+    }
+}
+
