d454de97dbda130f7d532f2eb0a4cc9b05d6328d
divan <hlinakcurbag@gmail.com>
2023-09-30 11:56:31 +0300

    pz 11 is done on 1 out of 5

--- src/pz_11_1.java
+++ src/pz_11_1.java
@@ -1,2 +1,22 @@
+// factorial
+// Напишите рекурсивную функцию factorial, которая принимает как аргумент одно неотрицательное целое число
+// и возвращает его значение. Напоминаю, что 0! = 1.
+
+import java.util.Scanner;
+
 public class pz_11_1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите одно неотрицательное целое число: ");
+        int number = scanner.nextInt();
+
+        System.out.println("Факториал числа " + number + " равен " + factorial(number));
+    }
+
+    public static int factorial(int number) {
+        if (number < 0) return 0;  // если чел глупый или тестировщик введёт отрицательное число
+        else if (number == 0) return 1;  // ваша напоминалка
+        else return number * factorial(number - 1);  // наша рекурсия
+    }
 }
