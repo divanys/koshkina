e244ff092fb8b549bd1e9f9b66416d3b8987ca36
divan <hlinakcurbag@gmail.com>
2023-10-03 15:56:34 +0300

    working with classes and objects

---
+++ src/testing_project/tweNew.java
@@ -0,0 +1,39 @@
+package testing_project;
+
+public class tweNew {
+    public static void main(String[] args) {
+        Person1 kate = new Person1("Kate", 30);
+        System.out.println(kate.getAge());
+        kate.setAge(33);
+        System.out.println(kate.getAge());
+        kate.setAge(123450);
+        System.out.println(kate.getAge());
+    }
+}
+
+class Person1 {
+    private String name;
+    private int age = 1;
+
+    public Person1(String name, int age) {
+        setName(name);
+        setAge(age);
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setAge(int age) {
+        if (age > 0 && age < 110)
+            this.age = age;
+    }
+}
