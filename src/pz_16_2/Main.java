3b5445b160d61f08a2f24c8714bfd4cbcfe6dcda
divan <hlinakcurbag@gmail.com>
2023-10-19 16:09:19 +0300

    Ticket is done

--- src/pz_16_2/Main.java
+++ src/pz_16_2/Main.java
@@ -1,4 +1,11 @@
 package pz_16_2;
 
 public class Main {
+    public static void main(String[] args) {
+        Spectator spectator1 = new Spectator("Павлова", "Валерия", "Алексеевна", 18);
+        Performance performance1 = new Performance("Спектакль", "Бархатные ручки", 16);
+        Ticket ticket1 = new Ticket(SessionType.WEEKDAY, 700, "Амфитеатр", 3, 16);
+        ticket1.regTicket(1, spectator1, performance1);
+        ticket1.printTicket(1);
+    }
 }
--- src/pz_16_2/Ticket.java
+++ src/pz_16_2/Ticket.java
@@ -1,16 +1,29 @@
 package pz_16_2;
 
 class Ticket implements TicketPricing {
+    private int numberTicket;
     private SessionType sessionType;
     private Spectator spectator;
+    private double basePrice;
     private double price;
     private double discount;
+    private String typeOfSeat;
+    private int numberOfRow;
+    private int numberOfSeat;
+    private String lastName;
+    private String middleName;
+    private String firstName;
+    private int ageRestriction;
+    private String showName;
+    private String showType;
+    private boolean isReg = false;
 
-    public Ticket(SessionType sessionType, String showName, Spectator spectator, double price, double discount) {
+    public Ticket(SessionType sessionType, double basePrice, String typeOfSeat, int numberOfRow, int numberOfSeat) {
         setSessionType(sessionType);
-        setSpectator(spectator);
-        setPrice(price);
-        setDiscount(discount);
+        setBasePrice(basePrice);
+        setTypeOfSeat(typeOfSeat);
+        setNumberOfRow(numberOfRow);
+        setNumberOfSeat(numberOfSeat);
     }
 
     public SessionType getSessionType() {
@@ -29,6 +42,14 @@ class Ticket implements TicketPricing {
         this.spectator = spectator;
     }
 
+    public double getBasePrice() {
+        return basePrice;
+    }
+
+    public void setBasePrice(double basePrice) {
+        this.basePrice = basePrice;
+    }
+
     public double getPrice() {
         return price;
     }
@@ -45,27 +66,136 @@ class Ticket implements TicketPricing {
         this.discount = discount;
     }
 
+    public String getTypeOfSeat() {
+        return typeOfSeat;
+    }
+
+    public void setTypeOfSeat(String typeOfSeat) {
+        this.typeOfSeat = typeOfSeat;
+    }
+
+    public int getNumberOfRow() {
+        return numberOfRow;
+    }
+
+    public void setNumberOfRow(int numberOfRow) {
+        this.numberOfRow = numberOfRow;
+    }
+
+    public int getNumberOfSeat() {
+        return numberOfSeat;
+    }
+
+    public void setNumberOfSeat(int numberOfSeat) {
+        this.numberOfSeat = numberOfSeat;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public int getAgeRestriction() {
+        return ageRestriction;
+    }
+
+    public void setAgeRestriction(int ageRestriction) {
+        this.ageRestriction = ageRestriction;
+    }
+
+    public String getShowName() {
+        return showName;
+    }
+
+    public void setShowName(String showName) {
+        this.showName = showName;
+    }
+
+    public String getShowType() {
+        return showType;
+    }
+
+    public void setShowType(String showType) {
+        this.showType = showType;
+    }
+
+    public int getNumberTicket() {
+        return numberTicket;
+    }
+
+    public void setNumberTicket(int numberTicket) {
+        this.numberTicket = numberTicket;
+    }
+
     // регистрация билетика
-    public void regTicket(int numberTicket, Spectator spectator) {
+    public void regTicket(int numberTicket, Spectator spectator, Performance performance) {
+        if (!isReg) {
+            setNumberTicket(numberTicket);
+            setShowName(performance.getShowName());
+            setLastName(spectator.getLastName());
+            setFirstName(spectator.getFirstName());
+            setMiddleName(spectator.getMiddleName());
+            setAgeRestriction(performance.getAgeRestriction());
+            setDiscount(sessionType.getDiscount());
+            calculateTicketPrice(sessionType, spectator.getAge(), getBasePrice());
 
+            System.out.println("Регистрация прошла успешно!\n");
+            printTicket(numberTicket);
+            System.out.println("Приятного просмотра!\n");
+            isReg = true;
+        } else {
+            System.out.println("Билет с такими данными уже зарегистрирован!");
+            printTicket(numberTicket);
+        }
+
+    }
+
+    // вывод самого билетика по номеру
+    public void printTicket(int numberTicket) {
         System.out.printf("Билет номер %d\nНазвание представления %s\nФамилия %s\nИмя %s\nОтчество %s\n" +
-                "Возрастное ограничение %s\n<Тип посадочного места> %s\nРяд %d\nМесто %d\nСтоимость %.2f\n" +
-                "Скидка %s\n\nПриятного просмотра!\n", numberTicket, getShowName(), spectator.getFirstName(),
-                spectator.getLastName(), spectator.getMiddleName(),);
+                        "Возрастное ограничение %s\n<Тип посадочного места> %s\nРяд %d\nМесто %d\nСтоимость базовая %.2f\n" +
+                        "Ваша скидка %s\nСтоимость вашего билета %.2f\n\n", numberTicket, getShowName(),
+                getLastName(), getFirstName(), getMiddleName(), getAgeRestriction(), getTypeOfSeat(), getNumberOfRow(),
+                getNumberOfSeat(), getBasePrice(), sessionType.getDiscount(),
+                calculateTicketPrice(sessionType, getAgeRestriction(), getBasePrice()));
     }
 
-    // вывод самого билетика для конкретного пользователя
-    public void printTicket(String firstName) {
-
+    // ну че удалим Леркин интересный выходной?
+    public void removeTicket(int numberTicket) {
+        if (isReg && this.numberTicket == numberTicket) {
+            this.numberTicket = 0;
+            isReg = false;
+            System.out.printf("Билет с номером %d удален.\n", numberTicket);
+        } else {
+            System.out.printf("Билет с номером %d не зарегистрирован!\n", numberTicket);
+        }
     }
-
 
     @Override
     public double calculateTicketPrice(SessionType sessionType, int age, double basePrice) {
         switch (sessionType) {
             case WEEKDAY:
             case WEEKEND:
-                price = basePrice * sessionType.getDiscount();
+                price = basePrice - basePrice * sessionType.getDiscount();
                 break;
             case HOLIDAY:
                 price = basePrice;
